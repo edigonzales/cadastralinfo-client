39cec2c54ed4a74259c552994fddd4e72e49e50c
edigonzales <edi.gonzales@gmail.com>
2021-08-15 18:20:05 +0200

    fix logo

--- cadastralinfo-client/src/main/java/ch/so/agi/cadastralinfo/App.java
+++ cadastralinfo-client/src/main/java/ch/so/agi/cadastralinfo/App.java
@@ -172,10 +172,9 @@ public class App implements EntryPoint {
         body().add(container);
 
         Location location = DomGlobal.window.location;
-
         HTMLElement logoDiv = div().css("logo")
                 .add(div()
-                        .add(img().attr("src", DomGlobal.window.location.href + "Logo.png").attr("alt", "Logo Kanton")).element()).element();
+                        .add(img().attr("src", location.protocol + "//" + location.host + "/" + location.pathname + "/Logo.png").attr("alt", "Logo Kanton")).element()).element();
         container.appendChild(logoDiv);
         
         HTMLElement searchContainerDiv = div().id("search-container").element();
